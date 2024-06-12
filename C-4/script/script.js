195a8870d32870df9dcd2241b7fefcc021a98775
tyeebang <111326879+tyeebang@users.noreply.github.com>
2024-06-12 13:06:47 +0900

    6/12 B유형 자습

--- C-4/script/script.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    $('#no-list li:first-child').click(function() {
-        $('#pop').css('display', 'flex')
-    })
-    $('#pop button').click(function() {
-        $('#pop').css('display', 'none')
-    })
-}
